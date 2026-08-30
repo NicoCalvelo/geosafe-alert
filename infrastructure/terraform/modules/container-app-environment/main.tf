19a97a1c31a38370740ce2a93a6dce02cfd16070
yoann cabrol <52161115+cabrol-yoann@users.noreply.github.com>
2026-08-30 17:15:28 +0200

    Revert "Feature/deploiment terraform"

--- infrastructure/terraform/modules/container-app-environment/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_container_app_environment" "this" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  log_analytics_workspace_id = var.log_analytics_workspace_id
-  infrastructure_subnet_id = var.infrastructure_subnet_id
-  tags = var.tags
-}
