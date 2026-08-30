830bc6474ce353376b89ab55ab13e81cc656a341
YOCA <cabrolyoann@hotmail.fr>
2026-08-30 17:43:51 +0200

    correction déploiement

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3333',
+  apiUrl: '',
   cesiumIonToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlNzhjZmQwYi1jODY0LTQ2NzUtYjNjYi0xMWIwYjJjOThiYzQiLCJpZCI6NDE4MDMwLCJpYXQiOjE3NzYxNjM0NTF9.8pMVkVuLkgpT1HbLpR4i-oOcdW5jn8-UIi7gGLuSV1Y'
 };
